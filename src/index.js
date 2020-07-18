535ebf79ffe8e25dc1fd00bf5582791952f64b6e
Leighton A Pitman <leightonpitman14@gmail.com>
2020-07-18 11:20:00 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>My ~boujie~ App</h1>;
+};
+const root = document.querySelector("#root");
+ReactDOM.render(<App />, root);
